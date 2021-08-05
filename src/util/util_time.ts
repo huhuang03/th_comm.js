47a0362257d0c1c1e2b0049fa806cc7ba396444c
huhuang03 <huhuang03@gmail.com>
2021-08-05 13:51:36 +0800

    add ts

---
+++ src/util/util_time.ts
@@ -0,0 +1,12 @@
+/**
+ * return the str time passed
+ * 刚刚 < 1分
+ * xx分前 < 1小时
+ * xx小时前 < 24小时
+ * x月x日 this year
+ * x年x月x日 not this year
+ * @param src_time the need calc time
+ */
+function time_passed(src_time: Date): string {
+  return ""
+}
