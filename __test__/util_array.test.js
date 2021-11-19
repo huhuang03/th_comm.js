60e0efc55782f8244f7605699238edd27efd47fc
huwenfan <huhuang03@gmail.com>
2021-11-19 15:35:25 +0800

    up

--- __test__/util_array.test.js
+++
@@ -1,9 +0,0 @@
-import { fillTo } from '../lib/index';
-test('normal fill to', () => {
-    const arr = [1, 2,];
-    const _rst = [1, 2, 0, 0, 0];
-    fillTo(arr, 0, 5);
-    expect(!(arr > _rst || _rst > arr)).toBe(true);
-    expect(arr.length).toBe(5);
-});
-//# sourceMappingURL=util_array.test.js.map
--- __test__/util_format.test.js
+++
@@ -1,5 +0,0 @@
-import { formatSize } from '../lib/index';
-test('test format1', () => {
-    expect(formatSize(1024, 1)).toBe('1KB');
-});
-//# sourceMappingURL=util_format.test.js.map
