352aa6e21d48c05d0916d42ca3236b48c51782dd
huwenfan <huhuang03@gmail.com>
2021-11-19 16:07:20 +0800

    up

--- __test__/util_array.test.ts
+++ __test__/util_array.test.ts
@@ -4,6 +4,16 @@ test('normal fill to', () => {
   const arr = [1, 2,];
   const _rst = [1, 2, 0, 0, 0];
   fillTo(arr, 0, 5);
+  console.log(arr);
   expect(!(arr > _rst || _rst > arr)).toBe(true);
   expect(arr.length).toBe(5);
+})
+
+test('normal fill not enough', () => {
+  const arr = [1, 2];
+  const _rst = [1, 2];
+  fillTo(arr, 0, 2);
+  console.log(arr);
+  expect(!(arr > _rst || _rst > arr)).toBe(true);
+  expect(arr.length).toBe(2);
 })
