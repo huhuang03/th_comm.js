57cb4f3d97deadb682e8de4f534c9b60c5f37eac
huwenfan <huhuang03@gmail.com>
2021-11-19 16:21:24 +0800

    up

--- __test__/util_array.test.ts
+++ __test__/util_array.test.ts
@@ -2,11 +2,15 @@ import {fillTo} from '../lib/index';
 
 test('normal fill to', () => {
   const arr = [1, 2,];
+  const arrDump = [...arr];
   const _rst = [1, 2, 0, 0, 0];
-  fillTo(arr, 0, 5);
-  console.log(arr);
-  expect(!(arr > _rst || _rst > arr)).toBe(true);
-  expect(arr.length).toBe(5);
+
+  const newArr = fillTo(arr, 0, 5);
+  expect(newArr.length).toBe(5);
+  expect(!(newArr > _rst || _rst > newArr)).toBe(true);
+
+  expect(arr.length).toBe(2);
+  expect(!(arr > arrDump || arr > arrDump)).toBe(true);
 })
 
 test('normal fill not enough', () => {
--- src/util/util_array.ts
+++ src/util/util_array.ts
@@ -1,4 +1,12 @@
 export function fillTo<T>(arr: Array<T>, val: T, to: number): Array<T> {
+  const rst = [...arr];
+  while (rst.length < to) {
+    rst.push(val);
+  }
+  return rst;
+}
+
+export function fillToInPlace<T>(arr: Array<T>, val: T, to: number): Array<T> {
   while (arr.length < to) {
     arr.push(val);
   }
